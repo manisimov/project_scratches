269c4347cc7a8c31d1a35fd2bb666ec0332ab9d3
Mikhail Anisimov <m.anisimov@email.cz>
2019-12-21 01:19:22 +0100

    Simple blinking demo for Nucleo-64 board

--- GCC_STM32L053/src/main.c
+++ GCC_STM32L053/src/main.c
@@ -8,10 +8,21 @@ void main()
 {
     SystemInit();
     SystemCoreClockUpdate();
+    RCC->IOPENR |= RCC_IOPENR_GPIOAEN;
+    GPIOA->MODER &= ~GPIO_MODER_MODE5_Msk;
+    GPIOA->MODER |= (0x01UL << GPIO_MODER_MODE5_Pos);
+    GPIOA->ODR |= GPIO_ODR_OD5;
+    
 
     while(1)
     {
-        test_u32 = SCB->CPUID;
-        test_u32 = 123;
+        for(test_u32 = 0; test_u32 < 50000; test_u32++)
+        { /* Delay */
+        }
+        GPIOA->ODR &= ~GPIO_ODR_OD5;
+        for(test_u32 = 0; test_u32 < 50000; test_u32++)
+        { /* Delay */
+        }
+        GPIOA->ODR |= GPIO_ODR_OD5;
     }
 }
